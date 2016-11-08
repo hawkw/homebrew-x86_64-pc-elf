bf0f4739b0017c12e554874a9bfb8ff12f6f5123
Hawk Weisman <hi@hawkweisman.me>
2016-11-08 18:12:12 -0500

    x86_64-pc-elf-gcc 5.4.0 (new formula)

---
+++ Formula/x86_64-pc-elf-gcc.rb
@@ -0,0 +1,41 @@
+class X8664PcElfGcc < Formula
+  desc "The GNU Compiler Collection"
+  homepage "https://gcc.gnu.org"
+  url "https://ftpmirror.gnu.org/gcc/gcc-5.4.0/gcc-5.4.0.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.4.0/gcc-5.4.0.tar.bz2"
+  sha256 "608df76dec2d34de6558249d8af4cbee21eceddbcb580d666f7a5a583ca3303a"
+
+  option "without-cxx", "Don't build the g++ compiler"
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+
+  depends_on "x86_64-pc-elf-binutils"
+
+  def install
+    # The C compiler is always built, C++ can be disabled
+    languages = %w[c]
+    languages << "c++" if build.with? "cxx"
+
+    mkdir "build" do
+      system "../configure", "--target=x86_64-pc-elf",
+                             "--prefix=#{prefix}",
+                             "--enable-languages=#{languages.join(",")}",
+                             "--disable-nls",
+                             "--without-headers",
+                             "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                             "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+                             "--with-mpc=#{Formula["libmpc"].opt_prefix}"
+
+      ENV.deparallelize
+      system "make", "all-gcc"
+      system "make", "all-target-libgcc"
+      system "make", "install-gcc"
+      system "make", "install-target-libgcc"
+    end
+    # info and man7 files conflict with native gcc
+    info.rmtree
+    man7.rmtree
+  end
+end
